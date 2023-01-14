0f225422d1334557a3b1d1c72a03b012dbf404e7
Admin <makwanadharmishtha44@gmail.com>
2023-01-14 15:30:04 +0530

    colour

---
+++ android/app/src/main/kotlin/com/example/colour_genret/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.colour_genret
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:colour_genret/colour.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    ),
+  );
+}
